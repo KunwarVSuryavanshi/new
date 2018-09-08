fc380aaf40c868298d9a18f01ce84c594a705c8d
KvS <35609032+KunwarVSuryavanshi@users.noreply.github.com>
2018-09-09 01:02:41 +0530

    Update parin.cpp

--- cppVSjava/Parameterized_inheritance/parin.cpp
+++ cppVSjava/Parameterized_inheritance/parin.cpp
@@ -1,1 +1,35 @@
+#include <iostream.h>
+#include <conio.h>
+class stud
+{
+private:
+	int s_no;
+public:
+	stud()
+	{
+		cout<<"Default constructor called...\n";
+	}
+	stud(int s_no)
+	{
+		cout<<"Stud class constructor...\n";
+		this->s_no=s_no;
+	}
+} ;
+class marks:public stud
+{
+private:
+	int er_no;
+public:
+	marks(int er_no,int no)
+	{
+		stud::stud(no);
+		this->er_no=er_no;
+		cout<<"Base class constructor called..\n";
+	}
+} ;
 
+void main()
+{
+	marks ob1(10,20);
+	getch();
+}
